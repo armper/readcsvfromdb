169cab8b10d31d94989006e0f6615e96b65f9236
Armando Perea <Armando.Perea@noaa.gov>
2017-10-20 13:07:11 -0400

    updated

--- src/main/java/com/csc/fsg/bpo/readcsv/ReadcsvApplication.java
+++ src/main/java/com/csc/fsg/bpo/readcsv/ReadcsvApplication.java
@@ -21,98 +21,106 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 @SpringBootApplication
 public class ReadcsvApplication implements CommandLineRunner {
 
-    private static final Logger log = LoggerFactory.getLogger(ReadcsvApplication.class);
+	private static final Logger log = LoggerFactory.getLogger(ReadcsvApplication.class);
 
-    public static void main(String args[]) {
-        SpringApplication.run(ReadcsvApplication.class, args);
-    }
+	public static void main(String args[]) {
+		SpringApplication.run(ReadcsvApplication.class, args);
+	}
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-    
-	private int i=0;
-	
-	private static final Object [] FILE_HEADER = {"agentNumber","firstName","lastName","companyId","statusId"};
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 
-	private static final String NEW_LINE_SEPARATOR = "\n";	
-	
-    @Override
-    public void run(String... strings) throws Exception {
-      
-        // Split up the array of whole names into an array of first/last names
-        List<String> agentNumbers = new ArrayList<>();
-        
-        Reader in = new FileReader("X:/thefile.csv");
-        Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);             
-        
-        records.forEach(record->{
-        	String agentNumber = record.get(0);
-        	agentNumbers.add(agentNumber);
-        	System.out.println("read agent no: "+agentNumber);
-        });
+	private int i = 0;
 
-        String sql = "select agent_number, search_first_name, search_last_name, company_id, status_id "
-        		+ "from cyberlife.agent "
-        		+ "inner join client.party p on cyberlife.agent.client_id=p.client_id "
-        		+ "where agent_number= ?";
-        
-		List<Agent> agentList=new ArrayList<Agent>();
-		
-    	System.out.println("Now querying database!");
-    
-		agentNumbers.forEach(agentNumber->{
-			 Agent agent = jdbcTemplate.queryForObject(
-	    			sql,  new Object[] {agentNumbers},
-	    			new BeanPropertyRowMapper<Agent>(Agent.class));
+	private static final Object[] FILE_HEADER = { "company", "agentNumber", "status", "firstName", "lastName",
+			"companyId", "statusId" };
+
+	private static final String NEW_LINE_SEPARATOR = "\n";
+
+	@Override
+	public void run(String... strings) throws Exception {
+
+		// Split up the array of whole names into an array of first/last names
+		List<AgentIn> agentsIn = new ArrayList<>();
+
+		Reader in = new FileReader("./thefile.csv");
+		Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+
+		for (CSVRecord record : records) {
+			AgentIn agentIn = new AgentIn(record.get(0), record.get(1), record.get(2));
+			agentsIn.add(agentIn);
+			System.out.println("read agent no: " + agentIn.getAgentNumber());
+		}
+
+		String sql = "select agent_number, search_first_name, search_last_name, company_id, status_id "
+				+ "from cyberlife.agent " + "inner join client.party p on cyberlife.agent.client_id=p.client_id "
+				+ "where agent_number= ?";
+
+		List<AgentOut> agentsOut = new ArrayList<>();
+
+		System.out.println("Now querying database!");
+
+		for (AgentIn agentIn : agentsIn) {
+
+			Agent agent = new Agent();
 			
-			agentList.add(agent);
+			List<Agent> agentsQuery = jdbcTemplate.query(sql, new Object[] { agentIn.getAgentNumber() },
+					new BeanPropertyRowMapper<Agent>(Agent.class));
+
+			if (agentsQuery.size()>0) {
+				agent = agentsQuery.get(0);
+			}
+			
+			AgentOut agentOut = mergeAgents(agentIn, agent);
+
+			agentsOut.add(agentOut);
+
 			i++;
-        	System.out.println("Added agent "+i+"/"+agentNumbers.size());
+			System.out.println("Added agent " + i + "/" + agentsIn.size());
 
-		});
-		
-		agentList.forEach(agent->{
-			System.out.println("Agent info: "+agent.toString());
-		});
-		
-    	System.out.println("Now writing file!");
+		}
 
-    	
+		System.out.println("Now writing file!");
+
 		FileWriter fileWriter = null;
-		
+
 		CSVPrinter csvFilePrinter = null;
-		
-		//Create the CSVFormat object with "\n" as a record delimiter
-        CSVFormat csvFileFormat = CSVFormat.DEFAULT.withRecordSeparator(NEW_LINE_SEPARATOR);
-				
+
+		// Create the CSVFormat object with "\n" as a record delimiter
+		CSVFormat csvFileFormat = CSVFormat.DEFAULT.withRecordSeparator(NEW_LINE_SEPARATOR);
+
 		try {
-			
-			//initialize FileWriter object
+
+			// initialize FileWriter object
 			fileWriter = new FileWriter("agentOut.csv");
-			
-			//initialize CSVPrinter object 
-	        csvFilePrinter = new CSVPrinter(fileWriter, csvFileFormat);
-	        
-	        //Create CSV file header
-	        csvFilePrinter.printRecord(FILE_HEADER);
-			
-			//Write a new agent object list to the CSV file
-			for (Agent agent : agentList) {
+
+			// initialize CSVPrinter object
+			csvFilePrinter = new CSVPrinter(fileWriter, csvFileFormat);
+
+			// Create CSV file header
+			csvFilePrinter.printRecord(FILE_HEADER);
+
+			// "company", "agentNumber", "status", "firstName", "lastName",
+			// "companyId", "statusId"
+
+			// Write a new agent object list to the CSV file
+			for (AgentOut agentOut : agentsOut) {
 				List<String> agentDataRecord = new ArrayList<>();
-	            agentDataRecord.add(agent.getAgentNumber());
-	            agentDataRecord.add(agent.getSearchFirstName());
-	            agentDataRecord.add(agent.getSearchLastName());
-	            agentDataRecord.add(agent.getCompanyId());
-	            agentDataRecord.add(String.valueOf(agent.getStatusId()));
-	            csvFilePrinter.printRecord(agentDataRecord);
+				agentDataRecord.add(agentOut.getCompanyCode());
+				agentDataRecord.add(agentOut.getAgentNumber());
+				agentDataRecord.add(agentOut.getStatus());
+				agentDataRecord.add(agentOut.getSearchFirstName());
+				agentDataRecord.add(agentOut.getSearchLastName());
+				agentDataRecord.add(agentOut.getCompanyId());
+				agentDataRecord.add(String.valueOf(agentOut.getStatusId()));
+				csvFilePrinter.printRecord(agentDataRecord);
 			}
 
 			System.out.println("CSV file was created successfully !!!");
-			
+
 		} catch (Exception e) {
 			System.out.println("Error in CsvFileWriter !!!");
 			e.printStackTrace();
@@ -123,9 +131,14 @@ public class ReadcsvApplication implements CommandLineRunner {
 				csvFilePrinter.close();
 			} catch (IOException e) {
 				System.out.println("Error while flushing/closing fileWriter/csvPrinter !!!");
-                e.printStackTrace();
+				e.printStackTrace();
 			}
 		}
 
-    }
+	}
+
+	private AgentOut mergeAgents(AgentIn agentIn, Agent agent) {
+		return new AgentOut(agentIn.getCompanyCode(), agentIn.getStatus(), agentIn.getAgentNumber(),
+				agent.getSearchFirstName(), agent.getSearchLastName(), agent.getCompanyId(), agent.getStatusId());
+	}
 }
